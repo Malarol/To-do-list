f7ea22f8cdbe787f939a3b96393e1f9c8e3a3c65
Mario Østervold <marioostervold@gmail.com>
2025-11-14 10:26:43 +0100

    Jobbet videre

--- todo.js
+++ todo.js
@@ -1,50 +1,93 @@
-    class Task {
-      constructor(name, status, type) {
-        this.name = name;
-        this.status = status;
-        this.type = type;
-      }
+class Task {
+  constructor(name, status, type) {
+    this.name = name;
+    this.status = status;
+    this.type = type;
+  }
 
-      getName() {
-        return this.name.toString();
-      }
+  getName() {
+    return this.name.toString();
+  }
 
-      getType() {
-        return this.type.toString();
-      }
+  getType() {
+    return this.type.toString();
+  }
 
-      getCheckBoxName() {
-        return this.name.toString() + this.type.toString();
-      }
+  getCheckBoxName() {
+    return this.name.toString() + this.type.toString();
+  }
+
+  changeStatus(checkbox) {
+    if (checkbox.checked) {
+      this.status = true;
+    } 
+    else {
+      this.status = false;  
+    }
+  }
+}
+
+let tasks = [];
+
+let createBtn = document.getElementById("create");
+let popUpBtn = document.getElementById("OpenPopUp");
+
+createBtn.addEventListener("click", createTask);
+popUpBtn.addEventListener("click", taskMenu);
+
+function createTask() {
+  let newName = document.getElementById("name");
+  let newType = document.getElementById("taskType");
+
+  const newClass = new Task(newName.value, true, newType.value);
+  tasks.push(newClass);
+  
+  let visibility = document.getElementById("popup");
+  visibility.classList.remove("open");
+  visibility.classList.add("closed");
+
+  updateHTML();
+}
+
+function updateHTML() {
+  document.querySelector("main").innerText = ""
+  for (let i = 0; i < tasks.length; i++) {
+
+    const selector = tasks[i];
+
+    if (selector.status == false){
+      tasks.splice(i, 1)
     }
 
-    let tasks = [];
+    const newDiv = document.createElement("div");
+    newDiv.className = "task";
 
-    document.getElementById("create").addEventListener("click", createTask);
+    const newTaskName = document.createElement("p");
+    newTaskName.innerText = selector.getName();
 
-    function createTask() {
-      const newName = document.getElementById("name").value;
-      const newType = document.getElementById("taskType").value;
+    const newTaskType = document.createElement("p");
+    newTaskType.innerText = selector.getType();
 
-      const newClass = new Task(newName, true, newType);
-      tasks.push(newClass);
+    const newTaskBox = document.createElement("input");
+    newTaskBox.type = "checkbox";
+    newTaskBox.id = selector.getCheckBoxName();
 
-      const newDiv = document.createElement("div");
-      newDiv.className = "task";
+    newDiv.appendChild(newTaskName);
+    newDiv.appendChild(newTaskType);
+    newDiv.appendChild(newTaskBox);
 
-      const newTaskName = document.createElement("p");
-      newTaskName.innerText = newClass.getName();
+    document.querySelector("main").appendChild(newDiv);
+    console.log(tasks);
+  }
+}
 
-      const newTaskType = document.createElement("p");
-      newTaskType.innerText = newClass.getType();
+function taskMenu() {
+  let visibility = document.getElementById("popup")
+  visibility.classList.remove("closed")
+  visibility.classList.add("open")
+}
 
-      const newTaskBox = document.createElement("input");
-      newTaskBox.type = "checkbox";
-      newTaskBox.id = newClass.getCheckBoxName();
+function refresh () {
 
-      newDiv.appendChild(newTaskName);
-      newDiv.appendChild(newTaskType);
-      newDiv.appendChild(newTaskBox);
 
-      document.getElementById("main").appendChild(newDiv);
-    }
+  }
